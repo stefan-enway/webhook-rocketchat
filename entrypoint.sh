8bd56678cfd739113482e50a07d27875a7a5ca33
Stefan Sels <51324826+stefan-enway@users.noreply.github.com>
2019-09-12 20:49:08 +0200

    escaping

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-#MESSAGE=`echo -n $MESSAGE|python -c 'import json,sys; print(json.dumps(sys.stdin.read()))'`
+MESSAGE=`echo -n $MESSAGE|python -c 'import json,sys; print(json.dumps(sys.stdin.read()))'`
 
-echo curl -X POST -H "Content-Type: application/json" --data '{ "text": $MESSAGE }' $WEBHOOK_URL
+curl -X POST -H "Content-Type: application/json" --data '{ \"text\": $MESSAGE }' $WEBHOOK_URL
