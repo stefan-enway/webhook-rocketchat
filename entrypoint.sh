7054a9c497582185a853a1ef320f5caa9e7d3b02
Stefan Sels <51324826+stefan-enway@users.noreply.github.com>
2019-09-12 21:42:25 +0200

    its ugly and we know it!

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-MESSAGE=`echo -n $MESSAGE|python -c 'import json,sys; print(json.dumps(sys.stdin.read()))'`
+MESSAGE=`echo -n $MESSAGE|python -c 'import json,sys; print(json.dumps(sys.stdin.read()).replace(r"\n", r"\r\n"))'`
 
 curl -X POST -H "Content-Type: application/json" --data "{ \"text\": $MESSAGE }" $WEBHOOK_URL
