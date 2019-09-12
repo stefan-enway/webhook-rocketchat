1d8fab59aa20e849ac93457abce07094c923b3bb
Stefan Sels <51324826+stefan-enway@users.noreply.github.com>
2019-09-12 21:50:31 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
 
+echo $MESSAGE
 MESSAGE=`echo -n $MESSAGE|python -c 'import json,sys; print(json.dumps(sys.stdin.read()).replace(r"\n", r"\r\n"))'`
 
 curl -X POST -H "Content-Type: application/json" --data "{ \"text\": $MESSAGE }" $WEBHOOK_URL
