10654efd755fa29a81a10603d0f7e1a677947778
Stefan Sels <51324826+stefan-enway@users.noreply.github.com>
2019-09-13 09:17:46 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 
-curl -X POST -H "Content-Type: application/json" --data "{ \"text\": $MESSAGE }" $WEBHOOK_URL
+echo curl -X POST -H "Content-Type: application/json" --data "{ \"text\": $MESSAGE }" $WEBHOOK_URL
